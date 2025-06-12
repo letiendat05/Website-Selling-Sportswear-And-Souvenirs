07d66b4730a14b5c78258968babacc15f5910544
td.global <151906762+letiendat05@users.noreply.github.com>
2025-06-13 00:46:17 +0700

    Delete sports_shop_backend/backend directory

--- sports_shop_backend/backend/models/order.model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const orderSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  items: [{ product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product' }, quantity: Number }],
-  createdAt: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model('Order', orderSchema);
