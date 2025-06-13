2e9ce13c09ed9b1680787330c68c67fba3d07f8c
td.global <151906762+letiendat05@users.noreply.github.com>
2025-06-13 12:54:32 +0700

    Delete ecommerce_backend directory

--- ecommerce_backend/routes/authRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('../controllers/authController');
-
-router.post('/login', controller.login);
-
-module.exports = router;
--- ecommerce_backend/routes/orderRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const auth = require('../middleware/authMiddleware');
-const controller = require('../controllers/orderController');
-
-router.post('/', controller.createOrder);
-router.get('/', auth, controller.getOrders);
-
-module.exports = router;
