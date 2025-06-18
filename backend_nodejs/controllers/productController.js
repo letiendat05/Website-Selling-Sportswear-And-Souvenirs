70e38eb12c72ee43d3422bcb33af6eba56a4adf1
td.global <151906762+letiendat05@users.noreply.github.com>
2025-06-19 00:13:27 +0700

    Delete backend_nodejs directory

--- backend_nodejs/controllers/productController.js
+++
@@ -1,5 +0,0 @@
-const products = require('../data/sampleProducts.json');
-
-exports.getAllProducts = (req, res) => {
-  res.json(products);
-};
--- backend_nodejs/routes/orders.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { createOrder, getOrders } = require('../controllers/orderController');
-
-router.post('/', createOrder);
-router.get('/', getOrders); // For admin dashboard
-
-module.exports = router;
